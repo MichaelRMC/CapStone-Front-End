44a347ed2dcba028873cb05e338042c601e508fb
Michael Caldwell <michaelcaldwell@pursuit.org>
2024-03-10 15:41:27 -0400

    Update VideoList.jsx

--- front-end/src/Components/VideoList.jsx
+++ front-end/src/Components/VideoList.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import VideoCard from '../Components/VideoCard'
 
-export default function VideoList() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function VideoList ()
+{
+  const [videos, setVideos] = useState(null);
+
+	useEffect(() => {
+		fetch(`${API / videos}`)
+			.then((response) => response.json)
+			.then((videos) => {
+				setVideos(videos);
+			})
+			.catch((error) => console.error(error));
+	}, []);
+
+	return (
+		<section className='video-list'>
+			<div>
+				{videos?.map((video, id) => {
+					<VideoCard key={id} thumbnail={thumbnailUrl} id={id} />;
+				})}
+			</div>
+		</section>
+	);
 }
